0e9c40d15bcaa87a6cc4b47e2149b9ced9369530
Vicky Murugan <vickymurugan@Vickys-MacBook-Air.local>
2025-04-11 23:36:24 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import { Profile } from "./components/Profile";
+
 function App() {
   return (
     <>
-      <div className="navbar bg-base-300 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Build Buddies 💻</a>
-        </div>
-        <div className="flex gap-2">
-          <div className="dropdown dropdown-end mx-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://avatars.githubusercontent.com/u/100864050?s=96&v=4"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
