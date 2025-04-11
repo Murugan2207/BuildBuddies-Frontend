c9d84577dc10cb7877e5538abdbea233c8bcd0eb
Murugan2207 <vickyvenkat2207@gmail.com>
2025-04-11 23:46:43 +0530

    Updated CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0 w-full">
     <aside>
       <p>Copyright © {new Date().getFullYear()} - All right reserved by Build Buddies</p>
     </aside>
