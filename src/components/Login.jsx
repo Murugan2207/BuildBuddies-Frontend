821451f009464daaf2ad51497252a50537d3ae3e
Murugan2207 <vickyvenkat2207@gmail.com>
2025-04-15 09:33:52 +0530

    Login Page created

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Login() {
+
+    const [emailId,setEmailId]=useState("")
+    const[password,setPassword]=useState("")
+    const handleEmailid=(e)=>{
+        setEmailId(e.target.value)
+    }
+    const handlePassword=(e)=>{
+        setPassword(e.target.value)
+    }
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center my-10">
+      <div className="  card bg-base-200 w-96 shadow-sm justify-center">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email ID</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Please Enter Valid Email ID"
+              value={emailId}
+              onChange={handleEmailid}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Please Enter Valid Password"
+              value={password}
+              onChange={handlePassword}
+            />
+          </fieldset>
+          <div className="card-actions justify-center">
+            <button className=" btn btn-primary justify-center">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
